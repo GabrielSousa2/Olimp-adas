b61fb05c657099c9b3c7ab82b13bca0c35afd156
GabrielSousa2 <104863987+GabrielSousa2@users.noreply.github.com>
2024-09-06 19:43:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma, tornando-se um ícone da nova geração do skate.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira de destaque, conhecida por sua técnica apurada e determinação dentro do tatame. Ela conquistou diversas medalhas em competições nacionais e internacionais, sendo um dos principais nomes do judô brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoca judoka"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta brasileira que se destacou nas Olimpíadas de Tóquio 2020, conquistando a medalha de prata no individual geral. Sua versatilidade e força a tornaram uma das principais ginastas do mundo.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginástica ginastica ouro"
+  }
+];
+
+
+
+
